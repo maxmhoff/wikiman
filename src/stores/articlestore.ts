d64cd959b9f8b7aa8c4d9412d54646c61610a9d4
maxmhoff <maxmhoff@sol.dk>
2021-09-28 01:05:37 +0200

    Add typing & max_length cap for titles

--- src/stores/articlestore.ts
+++ src/stores/articlestore.ts
@@ -1,9 +1,41 @@
-import { writable } from "svelte/store";
+import { writable } from 'svelte/store';
+import type { Writable } from 'svelte/store';
 
-export const article = writable(null);
+interface WikiArticle {
+    content_urls: {
+        desktop: {
+            page: string,
+        },
+    },
+    description: string,
+    extract: string,
+    title: string,
+    thumbnail: {
+        height: number,
+        source: string,
+        width: number,
+    }
+}
+
+export const article: Writable<WikiArticle> = writable({
+    content_urls: {
+        desktop: {
+            page: '',
+        },
+    },
+    description: '',
+    extract: '',
+    title: '',
+    thumbnail: {
+        height: 0,
+        source: '',
+        width: 0,
+    }
+});
 
 export const fetchArticle = async () => {
+    const MAX_CHARACTERS = 34
     const response = await fetch('https://en.wikipedia.org/api/rest_v1/page/random/summary', { method: 'GET', redirect: 'follow'});
     const loadedArticle = await response.json()
-    article.set(loadedArticle);
+    loadedArticle.title.length > MAX_CHARACTERS ? await fetchArticle() : article.set(loadedArticle);
 }
